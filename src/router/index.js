3aa0db3c26ff384116a03720f240651b86c3cb1e
aeraf-hesabe <aeraf@hesabe.com>
2020-12-22 16:51:15 +0530

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Brazil from "../views/Brazil";
+import Hawaii from "../views/Hawaii";
+import Jamaica from "../views/Jamaica";
+import Panama from "../views/Panama";
 
 Vue.use(VueRouter);
 
@@ -9,6 +13,26 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/brazil",
+    name: "brazil",
+    component: Brazil
+  },
+  {
+    path: "/hawaii",
+    name: "hawaii",
+    component: Hawaii
+  },
+  {
+    path: "/jamaica",
+    name: "jamaica",
+    component: Jamaica
+  },
+  {
+    path: "/panama",
+    name: "panama",
+    component: Panama
   },
   {
     path: "/about",
